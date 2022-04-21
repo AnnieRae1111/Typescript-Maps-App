98b7e04ef1bee094805d677a2c0838b0b5abeba1
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-04-21 17:11:32 -0600

    added map to application

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import { User } from './User'
-import { Company } from './Company'
+// import { User } from './User'
+// import { Company } from './Company'
 
 
-const user = new User()
-console.log(user)
 
-const company = new Company()
-console.log(company)
 
-google
+new google.maps.Map(document.getElementById('map'), {
+  //this is the options object 
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  }
+})
+//need to pass in mapDiv argument
+//create an HTLM element to house the map and pass a reference to it as the first argument
